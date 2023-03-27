15ee268c46fd9e8746f04c3aaaea6a84e9ffbf7c
Edgar Gomez <edgargomezborboa@hotmail.com>
2023-03-27 14:38:17 -0700

    Adding bugs and code smell

--- ConsolaPrueba/Program.cs
+++ ConsolaPrueba/Program.cs
@@ -6,7 +6,29 @@ namespace ConsolaPrueba
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string name = Console.ReadLine();
+            if (name = "John")
+            {
+                Console.WriteLine("Hello John!");
+            }
+            else if (name = "Mary")
+            {
+                Console.WriteLine("Hello Mary!");
+            }
+            else
+            {
+                Console.WriteLine("Hello, what's your name?");
+            }
+            Console.ReadLine();
+        }   
+        public void DoSomething(int someValue)
+        {
+            string someString = someValue.ToString();
+            int anotherValue = Convert.ToInt32(someString);
+            // ... code that uses anotherValue ...
         }
+
     }
 }
+
